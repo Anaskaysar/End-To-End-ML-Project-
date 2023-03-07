5ea1fc993f3193c28c0b58b3712378e7ce0a7325
anaskaysar <kaysarul.apurba@northsouth.edu>
2023-03-07 21:04:11 +0600

    up

--- src/logger.py
+++ src/logger.py
@@ -13,7 +13,7 @@ logging.basicConfig(
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
 )
-
+#testing logger file
 
 # if __name__=='__main__':
 #     logging.info("Logging has started")
